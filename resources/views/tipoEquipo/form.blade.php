53dd059a864712c5fa73e35fee0a844ca26b0703
Angel Quenta <parecesino10@gmail.com>
2020-02-08 13:31:58 -0500

    tipoEquipo falta crud

--- resources/views/tipoEquipo/form.blade.php
+++ resources/views/tipoEquipo/form.blade.php
@@ -1,6 +1,6 @@
- <div class="form-group row">
-        <label for="nombre_tipo_equipo" class="col-lg-3 col-form-label requerido">Nombre</label>
-             <div class="col-sm-10">
-                      <input type="text" class="form-control" id="nombre_tipo_equipo" p laceholder="Tipo Equipo" required>
-              </div>
+<div class="form-group row">
+ <label for="nombre_tipo_equipo" class="col-lg-3 col-form-label requerido">Nombre</label>
+ <div class="col-lg-10">
+  <input type="text" class="form-control" id="nombre_tipo_equipo" name="nombre_tipo_equipo" value="{{old('nombre_tipo_equipo',$data->nombre_tipo_equipo ?? '')}}" required>
+ </div>
 </div>
--- resources/views/tipoEquipo/itipoEquipo.blade.php
+++ resources/views/tipoEquipo/itipoEquipo.blade.php
@@ -1,21 +1,54 @@
-@extends('../layouts.app')
-@section('content')
-<header class="tipoequipo-titulo">Tipo Equipos</header>
-<div class="list-sede">
-  <a href="{{route('tipoequipo.create')}}">Añadir tipo Equipo</a>
-<div>
-  <table style="width:100%">
+@extends("theme.$theme.layout")
+@section('titulo')
+Tipo Equipos
+@endsection
+
+@section("scripts")
+<script src="{{asset("assets/pages/scripts/index.js")}}" type="text/javascript"></script>
+@endsection
+
+@section('contenido')
+<div class="row">
+ <div class="col-lg-12">
+  @include('includes.mensaje')
+  <div class="card">
+   <div class="card-header with-border">
+    <h3 class="card-title">Tipo Equipos</h3>
+    <div class="card-tools pull-right">
+     <a href="{{route('servicio.create')}}" class="btn btn-block btn-success btn-sm">
+      <i class="fa fa-fw fa-plus-circle"></i> Nuevo registro
+     </a>
+    </div>
+   </div>
+   <div class="card-body">
+    <table class="table table-striped table-bordered table-hover" id="tabla-data">
+     <thead>
       <tr>
-      <th>Nº</th>
-      <th>Tipo Equipo</th>
-    </tr>
-    @foreach ($tipo_equipos as $item)
-    <tr>
-      <td>{{$item->id_tipo_equipo}}</td>
-      <td>{{$item->nombre_tipo_equipo}}</td>
-    </tr>
-    @endforeach
-  </table>
-</div>
+       <th>Nombre</th>
+       <th class="width70"></th>
+      </tr>
+     </thead>
+     <tbody>
+     @foreach ($itipoEquipo as $data)
+      <tr>
+       <td>{{$data->nombre_tipo_equipo}}</td>
+       <td>
+        <a href="{{route('servicio.edit', $data->id_tipo_equipo)}}" class="btn-accion-tabla tooltipsC" title="Editar este registro">
+         <i class="fa fa-fw  fa-edit"></i>
+        </a>
+        <form action="{{route('tipoEquipo.destroy', $data->id_tipo_equipo)}}" class="d-inline form-eliminar" method="POST">
+         {{ csrf_field() }} @method("delete")
+         <button type="submit"  class="btn-accion-tabla eliminar tooltipsC" title="Eliminar este registro">
+          <i class="fa fa-fw fa-trash text-danger"></i>
+         </button>
+        </form>
+       </td>
+      </tr>
+     @endforeach
+     </tbody>
+    </table>
+   </div>
+  </div>
+ </div>
 </div>
 @endsection
